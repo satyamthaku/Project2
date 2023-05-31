b347e606acf639993ec2927ea73516f32c1f29fd
satyamthaku <satyamthakur880@gmail.com>
2023-05-31 18:47:55 +0530

    Values changed

--- product.java
+++ product.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 class Portfolio {
     public static void main(String args[]) {
-        int a = 7;
-        int b = 3;
+        int a = 8;
+        int b = 5;
         int c = a * b;
         System.out.println(c);
     }
